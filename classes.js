78837cf8c963e1e5ce7dcdec335d67d8eefc35b6
georgehardy <george.hardy@gmail.com>
2018-06-30 10:33:50 +1200

    Make circle green

--- classes.js
+++ classes.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one();
+  two();
+  three();
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -16,6 +18,14 @@ function one () {
 
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
+}
+
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
 }
 
 // CREATE FUNCTION two HERE
